34d886be20434e2093811608882f3f603afa1727
IJM <ivan.jaraiz@gmail.com>
2020-05-27 00:32:14 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables_constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
